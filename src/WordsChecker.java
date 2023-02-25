92f8e205396d8c6806371522c48fd85d9bf8b664
ConstDet <constx7@gmail.com>
2023-02-25 13:20:27 +0300

    Доработал WordChecker

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,23 +1,16 @@
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class WordsChecker {
-    private String text;
     private Set<String> set;
     public WordsChecker(String text) {
-        this.text = text;
-        this.set = new HashSet<>();
+        set = new HashSet<>();
         String[] lString = text.split("\\P{IsAlphabetic}+");
-        for (String s : lString) {
-            set.add(s);
-        }
+        Collections.addAll(set, lString);
     }
 
     public boolean hasWord(String word) {
-        if (set.contains(word)) {
-            return true;
-        } else return false;
+        return set.contains(word);
     }
 }
